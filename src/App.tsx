ccabb7323ac8d85b7b19026a29adf0f53076dbf4
muhammed mohsen <muhammedmohsen1111@gmail.com>
2022-10-16 15:04:51 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React,{ Fragment } from 'react'
+import Form from './Components/Form'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <div className="container">
+        <h1>Create Account</h1>
+        <div className="form_wrapper">
+          <Form />
+        </div>
+      </div>
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
